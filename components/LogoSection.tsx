8012a15d571131f20918aa7e72c0e8979ef9927f
v0agent <it+v0agent@vercel.com>
2026-06-23 12:18:51 +0000

    feat: replace SVG logo with image of Sierra Tel logo

--- components/LogoSection.tsx
+++ components/LogoSection.tsx
@@ -9,13 +9,16 @@ export function LogoSection() {
         gap: '16px',
       }}
     >
-      {/* Logo Icon */}
-      <svg width="70" height="55" viewBox="0 0 70 55" style={{ flexShrink: 0 }}>
-        {/* Blue triangular stripes */}
-        <polygon points="0,55 20,0 25,0 5,55" fill="#0096DD" />
-        <polygon points="18,55 38,0 43,0 23,55" fill="#0088CC" />
-        <polygon points="36,55 56,0 61,0 41,55" fill="#007BC0" />
-      </svg>
+      {/* Logo Image */}
+      <img
+        src="/sierra-tel-logo.png"
+        alt="Sierra Tel Logo"
+        style={{
+          height: '50px',
+          width: 'auto',
+          flexShrink: 0,
+        }}
+      />
 
       {/* Company Name with underline */}
       <div>
